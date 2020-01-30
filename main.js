e2f707c1037f1cbe957f251b1c4a28bd829cd9ff
Liza Star <lizadstar18@gmail.com>
2020-01-29 20:29:34 -0600

    made things invisible and visible

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
+const hideJumbo = () => {
+    document.getElementById('hogwartsJumbo').classList.add('invisible');
+    document.getElementById('studentForm').classList.remove('invisible');
+};
+document.getElementById("startSorting").addEventListener('click', hideJumbo);
+const generateStudent = () => {
+    let student = document.getElementById('firstYearsName').value;
+    document.getElementById('studentForm').classList.add('invisible');
+    console.log(student);
+}
+document.getElementById('sortButton').addEventListener('click', generateStudent);
 
-
-const sortingHat = [
-    
+const students = [
 ]
+// const printToDom = (divId, textToPrint) => {
+//     const selectedDiv = document.getElementById(divId);
+//     selectedDiv.innerHTML = textToPrint
+//   };
+//   const studentPrinter = (names) => {
+//     let domString = '';
+//     for(let i = 0; i< names.length; i++){
+//       domString += '<div class="col-md-6 col-lg-4 card-separation">'
+//       domString += '<div class="card">';
+//       domString += '  <div class="card-body">';
+//       domString += `    <h5 class="card-title">${names[i].name}</h5>`;
+//       domString += `    <p class="card-text">${names[i].house}</p>`;
+//       domString += `<button type="button" id="expel--${names[i].id}" class="btn btn-light">Expel</button>`
+//       domString += '  </div>';
+//       domString += '</div>';
+//       domString += '</div>';
+//     }
+//     printToDom('allStudents', domString);
+//   };
+//   studentPrinter(students)
+
+
