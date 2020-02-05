d09bf3525302f4aff4a8a80c2224239af0f75dd0
Liza Star <lizadstar18@gmail.com>
2020-02-04 18:28:26 -0600

    added color to cards and got expel button to work

--- main.js
+++ main.js
@@ -30,26 +30,53 @@ const students = []
 const printToDom = (divId, textToPrint) => {
 const selectedDiv = document.getElementById(divId);
 selectedDiv.innerHTML = textToPrint
+document.getElementById('firstYearsName').value = "";
 };
 const studentPrinter = (names) => {
 let domString = '';
 for(let i = 0; i < names.length; i++){
- domString += '<div class="col-md-6 col-lg-4 card-separation">'
- domString += '<div class="card">';
+    
+ domString += '<div id="cardSpacing" class="col-md-6 col-lg-4 card-separation">'
+ if (names[i].house === 'Gryffindor') {
+     domString += '<div class="card text-white bg-secondary mb-3">';}
+else if (names[i].house === 'Hufflepuff') {
+    domString += '<div class="card text-white bg-success mb-3">';}
+else if (names[i].house === 'Ravenclaw') {
+    domString += '<div class="card text-white bg-warning mb-3">';}
+else if (names[i].house === 'Slytherin') {
+    domString += '<div class="card text-white bg-info mb-3">';}  
  domString += '  <div class="card-body">';
  domString += `    <h5 class="card-title">${names[i].name}</h5>`;
  domString += `    <p class="card-text">${names[i].house}</p>`;
- domString += `<button type="button" id="expel--${names[i].id}" class="btn btn-light">Expel</button>`
+ domString += `<button type="button" id="${names[i].id}" class="expelButton btn btn-light">Expel</button>`
  domString += '  </div>';
  domString += '</div>';
  domString += '</div>';
  }
- document.getElementById('sortButton').addEventListener('click', expelStudent);
-const expelStudent = () => {
-    
-}
+ 
  printToDom('allStudents', domString);
+ activateExpel();
+ 
   };
+
+  const expelStudent = (e) => {
+    console.log(e);
+    console.log(e.target.id);
+    for (let i = 0; i < arrayOfStudents.length; i++){
+        if (e.target.id === arrayOfStudents[i].id){
+        arrayOfStudents.splice(i, 1);
+        };
+    };
+    studentPrinter(arrayOfStudents);
+    console.log(arrayOfStudents);
+};
+const activateExpel = () => {
+    let getButton = document.getElementsByClassName("expelButton");
+    for (let i = 0; i < getButton.length; i++){
+        getButton[i].addEventListener('click', expelStudent)
+    };
+};
+
 studentPrinter(students)
 
 
